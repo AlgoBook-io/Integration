a6c57316cbef753c5a7e1053d283cf06e2bf0a6a
Supratim Saha <supratim1703@gmail.com>
2020-02-15 21:52:42 +0530

    Updated Integration

--- divfloat.js
+++ divfloat.js
@@ -1,16 +1,17 @@
   $('document').ready(function(){
     $(window).scroll(function(){
-        var left_offset = $("#left").offset();
-        var right_offset = $("#right").offset();
-        
-        if ($(window).scrollTop() + $(window).height() > $("." + ($("#left").attr("class"))).offset().top) {
-            if ($("." + ($("#left").attr("class"))).offset().left < 0) {
-                $("#left").animate({"left": 0},2000);
-                $("." + ($("#left").attr("class"))).removeAttr("id");
-            }
-            else if ($("." + ($("#left").attr("class"))).offset().left > 0) {
-                $("#left").animate({"left": 0},2000);
-                $("." + ($("#left").attr("class"))).removeAttr("id");
+        var left_class = $("#left").attr("class");
+
+        if (typeof left_class !== typeof undefined && left_class !== false) {
+            if ($(window).scrollTop() + $(window).height() > $("." + (left_class)).offset().top) {
+                if ($("." + (left_class)).offset().left < 0) {
+                    $("#left").animate({"left": 0},2000);
+                    $("." + (left_class)).removeAttr("id");
+                }
+                else if ($("." + (left_class)).offset().left > 0) {
+                    $("#left").animate({"left": 0},2000);
+                    $("." + (left_class)).removeAttr("id");
+                }
             }
         }
     });
